4c112b8b923fd6580dfcb42d26ebb24dbbba94e8
박준서 <oponize@naver.com>
2023-03-08 21:30:42 +0900

    initialize

---
+++ apps/template/src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">
+        Hello world..!
+      </h1>
+      <p className="description">
+      </p>
+    </div>
+  );
+}
+
+export default App;
