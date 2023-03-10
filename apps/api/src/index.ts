22017d660ec5c9a5154252ea938d090c4bcb6a83
박준서 <oponize@naver.com>
2023-03-10 21:48:38 +0900

    feat: db init

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,7 @@
 import { createServer } from "./server";
 
 // eslint-disable-next-line turbo/no-undeclared-env-vars
-const port = process.env.PORT || 5001;
+const port = process.env.SERVER_PORT || 5001;
 const server = createServer();
 
 server.listen(port, () => {
